2210d6d5e2c37f80e74871f7930bd4456600e571
Dejan1990 <dejan.simo@hotmail.com>
2023-07-04 17:08:10 +0200

    Superglobals and Current Page Styling

--- index.php
+++ index.php
@@ -2,4 +2,16 @@
 
 $heading = 'Home';
 
+echo $_SERVER['REQUEST_URI'];
+
+function dd($value) {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+
+    die();
+}
+
+dd($_SERVER);
+
 require 'views/index.view.php';
