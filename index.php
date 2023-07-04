705bc865bc4b3cf3fdd16be280354a946de2d844
Dejan1990 <dejan.simo@hotmail.com>
2023-07-04 19:48:04 +0200

    Refactor

--- index.php
+++ index.php
@@ -6,10 +6,18 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 //dd($uri);
 
+/*
 if ($uri === '/php-server/') {
     require 'controllers/index.php';
 } else if ($uri === '/php-server/about') {
     require 'controllers/about.php';
 } else if ($uri === '/php-server/contact') {
     require 'controllers/contact.php';
-}
+}*/
+
+$routes = [
+    '/php-server/' => require 'controllers/index.php',
+    '/php-server/about' => require 'controllers/about.php',
+    '/php-server/contact' => require 'controllers/contact.php'
+];
+
