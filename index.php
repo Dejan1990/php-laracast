a174013b37eef2bd289e14e51ce35e3a03fe755e
Dejan1990 <dejan.simo@hotmail.com>
2023-07-04 15:36:37 +0200

    Associative Arrays

--- index.php
+++ index.php
@@ -1,3 +1,48 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        $books = [
+            [
+                'name' => 'Do Androids Dream of Electric Sheep',
+                'author' => 'Philip K. Dick',
+                'purchaseUrl' => 'http://localhost'
+            ],
+            [
+                'name' => 'The Langoliers',
+                'author' => 'Stephen King',
+                'purchaseUrl' => 'http://localhost'
+            ],
+            [
+                'name' => 'Project Hail Mary',
+                'author' => 'Andy Weir',
+                'purchaseUrl' => 'http://localhost'
+            ]
+        ];  
+    ?>
 
-echo "I am index file from root";
+    <ul>
+        <?php foreach ($books as $book): ?>
+            <li>
+                <a target="_blank" href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name'] ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
