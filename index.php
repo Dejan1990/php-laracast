80129a22802259784a17679837cf6c1f95acdf79
Dejan1990 <dejan.simo@hotmail.com>
2023-07-04 21:33:31 +0200

    Add routeToControll()

--- index.php
+++ index.php
@@ -4,22 +4,19 @@ require "functions.php";
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-//dd($uri);
-
-/*
-if ($uri === '/php-server/') {
-    require 'controllers/index.php';
-} else if ($uri === '/php-server/about') {
-    require 'controllers/about.php';
-} else if ($uri === '/php-server/contact') {
-    require 'controllers/contact.php';
-}*/
-
 $routes = [
     '/php-server/' => 'controllers/index.php',
     '/php-server/about' => 'controllers/about.php',
     '/php-server/contact' => 'controllers/contact.php'
 ];
+
+function routeToControll($uri, $routes) {
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
 
 function abort($code = 404) {
     http_response_code($code);
@@ -27,9 +24,7 @@ function abort($code = 404) {
     die();
 }
 
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-} else {
-    abort();
-}
+routeToControll($uri, $routes);
+
+
 
