a86ba743a375b1cdb3897a42989f44c1406aa175
Dejan1990 <dejan.simo@hotmail.com>
2023-07-06 12:39:41 +0200

    PDO First Steps

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 
 require "functions.php";
-require "router.php";
+//require "router.php";
 
+// connect to our MySQL database
+$dsn = "mysql:host=database;port=3306;dbname=laracast_demo_db;user=root;password=tiger;charset=utf8mb4";
 
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+//dd($posts);
+
+foreach ($posts as $post) {
+    echo '<li>' . $post['title'] . '</li>';
+}
 
