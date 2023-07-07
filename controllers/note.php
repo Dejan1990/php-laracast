fa16630a34e1e718ecf4885c23d1c3875866ce2d
Dejan1990 <dejan.simo@hotmail.com>
2023-07-07 19:10:42 +0200

    Authorization - add 403.php page

--- controllers/note.php
+++ controllers/note.php
@@ -5,15 +5,16 @@ $db = new Database($config['database']);
 
 $heading = 'Note';
 
-$note = $db->query("select * from notes where user_id = :user and id = :id", [
-        'user' => 1,
-        'id' => $_GET['id']
-    ])->fetch();
+$note = $db->query("select * from notes where id = :id", ['id' => $_GET['id']])->fetch();
 
 //dd($note);
 
 if (!$note) {
     abort();
+}
+
+if ($note['user_id'] != 1) {
+    abort(403);
 }
 
 require "views/note.view.php";
