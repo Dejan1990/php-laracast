e275d4cc7ef2fc10f7ebc65e71c2365296b9f5fd
Dejan1990 <dejan.simo@hotmail.com>
2023-07-07 15:40:09 +0200

    Fix missing parameter

--- index.php
+++ index.php
@@ -7,11 +7,17 @@ $config = require "config.php";
 
 $db = new Database($config['database']);
 
-$id = $_GET['id'];
+$id = $_GET['id'] ?? '';
 
-$query = "SELECT * FROM posts WHERE id = :id";
+if ($id) {
+    $query = "SELECT * FROM posts WHERE id = :id";
 
-$posts = $db->query($query, [':id' => $id])->fetch(); //: before id is optional
+    $posts = $db->query($query, [':id' => $id])->fetch(); // : before id is optional
+} else {
+    $query = "SELECT * FROM posts";
+
+    $posts = $db->query($query)->fetchAll(); // : before id is optional
+}
 
 dd($posts);
 
