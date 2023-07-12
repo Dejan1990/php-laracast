6c4de6f7a0a85d101c0b86453467c23f619c8f91
Dejan1990 <dejan.simo@hotmail.com>
2023-07-12 18:13:09 +0200

    Extract a Simple Validation Class - static method

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $validator = new Validator();
 
-    if (!$validator->string($_POST['body'], 1, 1000)) {
+    if (!Validator::string($_POST['body'], 1, 1000)) {
         $errors['body'] = 'A body of no more than 1,000 characters is required';
     }
 
